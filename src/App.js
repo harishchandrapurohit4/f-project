0591f243f849d59a54ec938ad682d88d9ea9ed3e
harishchandrapurohit4 <harishpurohit9475@gmail.com>
2025-12-20 13:02:53 +0530

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import Header from './components/header';
+import Nav from './components/nav';
+import Main from './components/main';
+import Footer from './components/footer';
 
 function App() {
-return(
-  <h1> Hellow World</h1>
-)
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/main.js
@@ -0,0 +1,13 @@
+function Main(){
+    return(
+        <main>
+            <h2>Welcome to Little Lemon</h2>
+      <p>
+        We are a family-owned Mediterranean restaurant focused on traditional
+        recipes served with a modern twist.
+      </p>
+        </main>
+    )
+}
+
+export default Main;
